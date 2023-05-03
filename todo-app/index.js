ad496c2d83a217bba0cf371bd117ca6d981b7fe8
Gayathri-69 <127761746+Gayathri-69@users.noreply.github.com>
2023-05-03 19:48:24 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,4 @@
-const app = require('./app');
-
-app.listen(env.port, () => {
-    console.log("Started express server at port 3000")
-})
+const app = require("./app"); 
+ app.listen(process.env.PORT || 3000, () => { 
+   console.log("Started express server "); 
+ }); 
